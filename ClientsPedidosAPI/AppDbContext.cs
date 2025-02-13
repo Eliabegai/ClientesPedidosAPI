9e0496eaecb94b36954836c9585764d7827b46fc
Eliabe Gai <eliabegai@gmail.com>
2025-02-13 16:33:46 -0300

    feat: context db

---
+++ ClientsPedidosAPI/AppDbContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+  public DbSet<Cliente> Clientes { get; set; }
+  public DbSet<Pedido> Pedido {get; set;}
+  public DbSet<ItemPedido> ItensPedido {get; set;}
+
+  public AppDbContext(DbContextOptions<AppDbContext> options): base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Cliente>()
+          .HasIndex(c => c.CPF)
+          .IsUnique();
+        
+        modelBuilder.Entity<Pedido>()
+          .HasOne(p => p.Cliente)
+          .WithMany(c => c.Pedidos)
+          .HasForeignKey(p => p.ClienteId);
+    }
+}
